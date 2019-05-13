13a40151c30ca75cd6aaed57f4f7cb2b826974af
NNCWRMWCWF <frolovav88@mail.ru>
2019-05-13 21:02:16 +0300

    Second commit

---
+++ tacos/src/main/java/tacos/Taco.java
@@ -0,0 +1,42 @@
+// tag::all[]
+// tag::allButValidation[]
+package tacos;
+import java.util.List;
+
+// end::allButValidation[]
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+// tag::allButValidation[]
+import lombok.Data;
+
+@Data
+public class Taco {
+
+  // end::allButValidation[]
+  @NotNull
+  @Size(min=5, message="Name must be at least 5 characters long")
+  // tag::allButValidation[]
+  private String name;
+  // end::allButValidation[]
+  @Size(min=1, message="You must choose at least 1 ingredient")
+  // tag::allButValidation[]
+  private List<String> ingredients;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public List<String> getIngredients() {
+	return ingredients;
+}
+public void setIngredients(List<String> ingredients) {
+	this.ingredients = ingredients;
+}
+  
+  
+
+}
+//end::allButValidation[]
+//tag::end[]
