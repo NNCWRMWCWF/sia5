0acd4c4cb9af670ac3330461b9bab45b6b2677f5
NNCWRMWCWF <frolovav88@mail.ru>
2019-05-13 20:59:39 +0300

    Initial commit

---
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -0,0 +1,54 @@
+package tacos;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+	public final String id;
+	public final String name;
+	public final Type type;
+	
+	
+	
+	public Ingredient(String id, String name, Type type) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+	}
+	
+	
+
+
+
+	public String getId() {
+		return id;
+	}
+
+
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+
+
+	public Type getType() {
+		return type;
+	}
+
+
+
+
+
+	public static enum Type{
+		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+	}
+	
+}
