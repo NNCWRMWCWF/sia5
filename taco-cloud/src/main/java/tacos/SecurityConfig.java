3e5fa36af834974e25e90410c57d1d83d161ccb0
frolov <frolov@iqmen.ru>
2019-08-06 09:10:29 +0300

    q

--- taco-cloud/src/main/java/tacos/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/SecurityConfig.java
@@ -1,19 +1,29 @@
 package tacos;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	DataSource dataSource;
+	private UserDetailsService userDetailsService;
+//	DataSource dataSource;
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new StandardPasswordEncoder("53cr3t");
+	}
+	
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,9 +35,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		.authorities("ROLE_USER");
 		
 		//jdbc-authentication
-		auth.jdbcAuthentication().dataSource(dataSource)
-		.usersByUsernameQuery("select username, password, enabled from Users where username=?")
-		.authoritiesByUsernameQuery("select username, authority from UserAuthorities where username=?");
+//		auth.jdbcAuthentication().dataSource(dataSource)
+//		.usersByUsernameQuery("select username, password, enabled from Users where username=?")
+//		.authoritiesByUsernameQuery("select username, authority from UserAuthorities where username=?");
+		
+		//jpa-authentication
+		auth.userDetailsService(userDetailsService)
+		.passwordEncoder(encoder());
 	}
 
 	
